ad1c703415db4cb7115d4224f0d26ecf52b7eb82
roshith-normality <roshith.krishna@normality.in>
2025-07-07 14:40:32 +0530

    finished-page-details

--- app/page.js
+++ app/page.js
@@ -5,6 +5,11 @@ export default function Home() {
   return (
     <div className="">
       <TopBar />
+      <div className="flex flex-col items-center justify-center bg-gray-100">
+        <h1 className="text-primary font-bold text-2xl">Finished Pages</h1>
+        <a href="/instructor/login" className="hover:text-primary">Login</a>
+        <a href="/instructor/signup " className="hover:text-primary">Signup</a>
+      </div>
       <Footer />
     </div>
   );
