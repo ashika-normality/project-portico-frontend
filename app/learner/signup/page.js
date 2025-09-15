842dc7c0292133129a92b03da29c9c215ed183c6
ashika-normality <ashika@normality.in>
2025-09-15 21:54:51 +0530

    Updated learner signup page and form V4.5

--- app/learner/signup/page.js
+++ app/learner/signup/page.js
@@ -2,7 +2,7 @@ import SignupForm from "./SignupForm";
 
 const Page = () => (
   <div className="w-full min-h-screen flex justify-center items-center bg-gray-100">
-    <div className="flex w-full max-w-6xl h-[500px] bg-white rounded-lg shadow-lg overflow-hidden">
+    <div className="flex w-full max-w-6xl bg-white rounded-lg shadow-lg overflow-hidden">
       
       {/* Left Image Section */}
       <div className="w-1/2 hidden md:flex items-center justify-center">
@@ -14,7 +14,7 @@ const Page = () => (
       </div>
 
       {/* Right Form Section */}
-      <div className="w-full md:w-1/2 flex flex-col justify-center px-10 py-8 overflow-y-auto">
+      <div className="w-1/2 flex flex-col justify-center px-10 py-8">
         <h1 className="font-raleway text-2xl font-bold text-primary text-center">Learner Sign Up</h1>
         <p className="font-source-sans text-greydarker text-center mb-4">
           Create your account and start learning with us!
